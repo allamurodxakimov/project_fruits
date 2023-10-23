a8127b27ddc1ae85c461ca926e311b92c5d17e7e
allamurodxakimov <xakimov2806@gmail.com>
2023-10-23 08:46:32 -0700

    get cheapest fruit

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -1,3 +1,5 @@
+from csv import reader
+import string
 def get_cheapest_fruit_id(data: str) -> int:
     """
     This function returns the index of the cheapest fruit
@@ -7,5 +9,19 @@ def get_cheapest_fruit_id(data: str) -> int:
     returns:
         int: id of the cheapest fruit
     """
-    # your code here
-    pass
+    f=open("fruits.csv")
+    data=reader(f)
+    ls=[]
+    for i in data:
+        ls.append(i[-1])
+    ls.remove(ls[0])
+    lst=[]
+    for i in range(len(ls)):
+        lst.append(float(ls[i]))
+    mi=lst[0]
+    for i in range(len(lst)):
+        if mi>=lst[i]:
+            mi=lst[i]
+    k=lst.index(int(mi))
+    return(k+2)
+print(get_cheapest_fruit_id("fruits.csv"))
